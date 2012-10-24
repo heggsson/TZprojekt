23afa932e189ae51ae65def2ac7d5ff991c23797
Herman Gustafsson <herman.gustafsson89@gmail.com>
2012-10-24 10:26:53 +0100

    Hermans startfiler för nodejs

---
+++ pie.js
@@ -0,0 +1,34 @@
+    
+
+
+      // Load the Visualization API and the piechart package.
+      google.load('visualization', '1.0', {'packages':['corechart']});
+
+      // Set a callback to run when the Google Visualization API is loaded.
+      google.setOnLoadCallback(drawChart);
+
+      // Callback that creates and populates a data table,
+      // instantiates the pie chart, passes in the data and
+      // draws it.
+      function drawChart() {
+
+        // Create the data table.
+        var data = new google.visualization.DataTable();
+        data.addColumn('string', 'Topping');
+        data.addColumn('number', 'Slices');
+        data.addRows([
+          ['Skinka', 3],
+          ['Salami', 1],
+          ['Leverpastej', 1],
+          ['Brieost', 1]
+        ]);
+
+        // Set chart options
+        var options = {'title':'Forsaljningsandelar charken ICA',
+                       'width':800,
+                       'height':600};
+
+        // Instantiate and draw our chart, passing in some options.
+        var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
